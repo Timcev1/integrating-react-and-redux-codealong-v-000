5f18649f186e76893297322b8c650fbb277fffc4
Timothy Cevallos <timcev@gmail.com>
2017-10-18 14:22:53 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import changeCount from './reducers/changeCount';
+import createStore from './createStore';
 
 export function render() {
   ReactDOM.render(
