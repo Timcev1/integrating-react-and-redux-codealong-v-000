3930a64a20c35dbf120b346eec399b29059d99f8
Timothy Cevallos <timcev@gmail.com>
2017-10-18 14:23:14 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import App from './App';
 import changeCount from './reducers/changeCount';
 import createStore from './createStore';
 
-  const store = createStore(changeCount);
+const store = createStore(changeCount);
+
 export function render() {
   ReactDOM.render(
-    <App />,
+   <App store={store} />,
     document.getElementById('root')
   );
 };
